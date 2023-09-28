9972224f72d7f90c0d6c6db5469c0e9cf61e7252
Tahani <t.alhammad@linq5.nl>
2023-09-28 21:44:39 +0200

    develop branch

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React from 'react';
+import React, { Component } from "react";
+import logo from "./logo.svg"; //later i waint to use my logo
+import "./App.css";
+import Todos from "./components/Todos"
 
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return ( 
+        <Todos />
+    );
+  }
 }
 
 export default App;
