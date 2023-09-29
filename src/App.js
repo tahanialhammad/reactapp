aeb6aded7a0162114eeccdcc339546fe0dcb2d6b
Tahani <t.alhammad@linq5.nl>
2023-09-29 16:01:24 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import logo from "./logo.svg"; //later i waint to use my logo
 import "./App.css";
 import Todos from "./components/Todos"
+import Social from './components/Social'
 
 class App extends Component {
   render() {
     return ( 
+      <div>
         <Todos />
+        <Social />
+      </div>
     );
   }
 }
