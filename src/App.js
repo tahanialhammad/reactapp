95fb2b0724c1a021a87948a237f16a4f78943bb8
Tahani <t.alhammad@linq5.nl>
2023-09-30 22:37:43 +0200

    update header class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"; //use class component instead of function 
 import logo from "./logo.svg"; //later i waint to use my logo
 import "./App.css";
 import Todos from "./components/Todos"
@@ -6,14 +6,26 @@ import Social from './components/Social'
 import Team from "./components/Team";
 import Header from "./components/Header";
 
+// was class App extends React.Component {...}
 class App extends Component {
+  method(pram){
+    return <h3>react method with: {pram}</h3>
+  }
+
   render() {
+    const siteInfo = 'Copy right for React App'; //for varblie
+    const reactMethod = this.method('pramters')
+
+    // render is a method
     return ( 
       <div>
-        <Header />
+        {/* props in class component */}
+        <Header logo="React AppS" />
         <Team />
         <Todos />
         <Social />
+        {reactMethod}
+        {siteInfo}
       </div>
     );
   }
