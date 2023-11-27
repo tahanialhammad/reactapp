c4940460fae6113b66f5ff455e306407f6515fc5
tahanialhammad <tahani_alhammad@yahoo.com>
2023-11-27 18:05:59 +0100

    install route

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -6,6 +6,9 @@ import {
   Route,
 } from 'react-router-dom';
 
+// switch is old , now is route https://kinsta.com/knowledgebase/attempted-import-error-switch-is-not-exported-from-react-router-dom/#:~:text=%2Ddom'%E2%80%9D%20Error-,What%20Causes%20the%20%E2%80%9C'Switch'%20is%20not%20exported%20from%20',with%20the%20component. 
+
+
 import App from './App';
 import Header from './Header';
 import About from '../pages/About';
